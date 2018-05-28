9492e0cdea013e1f0fec48ea23430b82e6b90fe6
Luis-Madrid <LuisMadrid87@gmail.com>
2018-05-28 12:56:00 -0700

    Grid output working

--- designs.js
+++ designs.js
@@ -1,7 +1,25 @@
-/* JavaScript function to create the grid when submit button is clicked.
- * It takes in height and width of the grid from the html form and creates
- * the grid on the table canvas element in html page.
- */
+//Javascript makegrid()
+document.querySelector("#submitted").addEventListener("click", makeGrid);
+
+
+
 function makeGrid() {
+  //document.body.style.backgroundColor = "red";
+
+  var height = document.getElementById("input_height").value;
+  var width = document.getElementById("input_width").value;
+  const table = document.getElementById("pixel_canvas");
+
+  event.preventDefault();
+
+  for (var x = 1; x <= height; x++) {
+    var newTr = document.createElement("tr");
+    table.appendChild(newTr);
+
+    for (var i = 1; i <= width; i++) {
+      var newTd = document.createElement("td");
+      newTr.appendChild(newTd);
+    }
+  }
 
 }
