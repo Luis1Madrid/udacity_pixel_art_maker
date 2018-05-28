b88d566a9fbb4d5dd77e68e8f473ff907d11fa6c
Luis-Madrid <LuisMadrid87@gmail.com>
2018-05-28 09:52:49 -0700

    Intial commit

---
+++ designs.js
@@ -0,0 +1,7 @@
+/* JavaScript function to create the grid when submit button is clicked.
+ * It takes in height and width of the grid from the html form and creates
+ * the grid on the table canvas element in html page.
+ */
+function makeGrid() {
+
+}
